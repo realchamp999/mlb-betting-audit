234d3d4f11995910e839bca771ee74dd4e033c53
realchamp999 <francisng811@gmail.com>
2026-07-03 21:20:14 -0700

    Add files via upload

--- mlb_streamlit_app.py
+++ mlb_streamlit_app.py
@@ -627,15 +627,9 @@ with tab2:
             return ""
 
         sum_df = pd.DataFrame(summary_rows)
-        # pandas >=2.1 renamed applymap to map
-        try:
-            styled = (sum_df.style
-                      .map(color_tier, subset=["Tier"])
-                      .map(color_result, subset=["Side Win%","Total Win%"]))
-        except AttributeError:
-            styled = (sum_df.style
-                      .applymap(color_tier, subset=["Tier"])
-                      .applymap(color_result, subset=["Side Win%","Total Win%"]))
+        styled = (sum_df.style
+                  .map(color_tier,   subset=["Tier"])
+                  .map(color_result, subset=["Side Win%","Total Win%"]))
         st.dataframe(styled, use_container_width=True, hide_index=True)
 
         st.subheader("📋 Full Log")
